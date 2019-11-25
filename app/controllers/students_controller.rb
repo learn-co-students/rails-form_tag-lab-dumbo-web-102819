fe70898691133ef015521c0ddbb675ac31b81a85
Frank Gerold <FrankETSGerold@gmail.com>
2019-11-25 12:18:06 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,21 @@
 class StudentsController < ApplicationController
-  
+
   def index
     @students = Student.all
   end
 
   def show
     @student = Student.find(params[:id])
+  end
+
+  def new
+    @student = Student.new()
+  end
+
+  def create
+    params.permit!
+    @student = Student.create(params[:student])
+    redirect_to '/students'
   end
 
 end
