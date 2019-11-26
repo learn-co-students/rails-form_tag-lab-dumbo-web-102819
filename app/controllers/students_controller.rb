8620bebe95ecac061af8c5e57823b1a723c86403
Soner Mezgitci <you@example.com>
2019-11-25 19:37:25 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -6,6 +6,18 @@ class StudentsController < ApplicationController
 
   def show
     @student = Student.find(params[:id])
+
   end
 
+  def new
+
+  end 
+
+
+  def create
+    @white = params.require(:student).permit(:first_name, :last_name)
+    @student = Student.create(@white)
+    redirect_to student_path(@student.id)
+  end 
+  
 end
